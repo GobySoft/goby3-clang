7e2aacb641a7908dadb1223748a2fbf311da1baf
Toby Schneider <toby@gobysoft.org>
2019-08-01 09:46:28 +0000

    Clean up CMakeLists.txt, bring in Goby3 and yaml-cpp as dependencies

--- tool.cpp
+++ tool.cpp
@@ -1,13 +1,13 @@
-// Declares clang::SyntaxOnlyAction.
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/Frontend/FrontendActions.h"
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
-// Declares llvm::cl::extrahelp.
+
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/raw_ostream.h"
 
-#include "clang/ASTMatchers/ASTMatchFinder.h"
-#include "clang/ASTMatchers/ASTMatchers.h"
+#include "goby/middleware/marshalling/interface.h"
 
 #include <iostream>
 #include <sstream>
@@ -17,39 +17,39 @@ using namespace llvm;
 using namespace clang;
 using namespace clang::ast_matchers;
 
-//
-
-template <const char* const* method>
-StatementMatcher pubsub_matcher = cxxMemberCallExpr(
-    expr().bind("pubsub_call_expr"),
-    // call is on an instantiation of a class derived from StaticTransporterInterface
-    on(expr(
-        anyOf(
-            // Thread: pull out what "this" in "this->interprocess()" when "this" is derived from goby::middleware::Thread
-            cxxMemberCallExpr(on(hasType(pointsTo(cxxRecordDecl(
-                cxxRecordDecl().bind("on_thread_decl"),
-                isDerivedFrom(cxxRecordDecl(hasName("::goby::middleware::Thread")))))))),
-            // also allow out direct calls to publish/subscribe
-            expr().bind("on_expr")),
-        hasType(cxxRecordDecl(
-            decl().bind("on_type_decl"),
-            isDerivedFrom(cxxRecordDecl(hasName("::goby::middleware::StaticTransporterInterface"))),
-            unless(hasName("::goby::middleware::NullTransporter")))))),
-    callee(cxxMethodDecl(
-        // "publish" or "subscribe"
-        hasName(*method),
-        // Group (must refer to goby::middleware::Group)
-        hasTemplateArgument(0, templateArgument(refersToDeclaration(varDecl(
-                                   hasType(cxxRecordDecl(hasName("::goby::middleware::Group"))),
-                                   // find the actual string argument and bind it
-                                   hasDescendant(cxxConstructExpr(hasArgument(
-                                       0, stringLiteral().bind("group_string_arg")))))))),
-        // Type (no restrictions)
-        hasTemplateArgument(1, templateArgument().bind("type_arg")),
-        // Scheme (must be int)
-        hasTemplateArgument(2,
-                            templateArgument(templateArgument().bind("scheme_arg"),
-                                             refersToIntegralType(qualType(asString("int"))))))));
+StatementMatcher pubsub_matcher(const char* method)
+{
+    return cxxMemberCallExpr(
+        expr().bind("pubsub_call_expr"),
+        // call is on an instantiation of a class derived from StaticTransporterInterface
+        on(expr(
+            anyOf(
+                // Thread: pull out what "this" in "this->interprocess()" when "this" is derived from goby::middleware::Thread
+                cxxMemberCallExpr(on(hasType(pointsTo(cxxRecordDecl(
+                    cxxRecordDecl().bind("on_thread_decl"),
+                    isDerivedFrom(cxxRecordDecl(hasName("::goby::middleware::Thread")))))))),
+                // also allow out direct calls to publish/subscribe
+                expr().bind("on_expr")),
+            hasType(cxxRecordDecl(decl().bind("on_type_decl"),
+                                  isDerivedFrom(cxxRecordDecl(
+                                      hasName("::goby::middleware::StaticTransporterInterface"))),
+                                  unless(hasName("::goby::middleware::NullTransporter")))))),
+        callee(cxxMethodDecl(
+            // "publish" or "subscribe"
+            hasName(method),
+            // Group (must refer to goby::middleware::Group)
+            hasTemplateArgument(0, templateArgument(refersToDeclaration(varDecl(
+                                       hasType(cxxRecordDecl(hasName("::goby::middleware::Group"))),
+                                       // find the actual string argument and bind it
+                                       hasDescendant(cxxConstructExpr(hasArgument(
+                                           0, stringLiteral().bind("group_string_arg")))))))),
+            // Type (no restrictions)
+            hasTemplateArgument(1, templateArgument().bind("type_arg")),
+            // Scheme (must be int)
+            hasTemplateArgument(
+                2, templateArgument(templateArgument().bind("scheme_arg"),
+                                    refersToIntegralType(qualType(asString("int"))))))));
+}
 
 class PubSubPrinter : public MatchFinder::MatchCallback
 {
@@ -81,22 +81,20 @@ class PubSubPrinter : public MatchFinder::MatchCallback
         else if (layer_type.find("InterVehicle") != std::string::npos)
             layer = "intervehicle";
 
-        //        if(on_thread_decl)
-        //            on_thread_decl->dump();
-
         std::string thread = "unknown";
         if (on_thread_decl)
         {
             // todo: see if there's a cleaner way to get this with the template parameters
             thread = on_thread_decl->getTypeForDecl()->getCanonicalTypeInternal().getAsString();
             std::string class_str = "class ";
-            if(thread.find(class_str) != std::string::npos)
+            if (thread.find(class_str) != std::string::npos)
                 thread = thread.substr(class_str.size());
         }
 
         const std::string group = group_string_lit->getString().str();
         const std::string type = type_arg->getAsType().getAsString();
-        const int scheme = scheme_arg->getAsIntegral().getExtValue();
+        const int scheme_num = scheme_arg->getAsIntegral().getExtValue();
+        const std::string scheme = goby::middleware::MarshallingScheme::to_string(scheme_num);
 
         // hide internal groups for now
         if (group.find("goby::") != std::string::npos)
@@ -144,8 +142,8 @@ int main(int argc, const char** argv)
     SubscribePrinter subscribe_printer;
     MatchFinder finder;
 
-    finder.addMatcher(pubsub_matcher<&publish_str>, &publish_printer);
-    finder.addMatcher(pubsub_matcher<&subscribe_str>, &subscribe_printer);
+    finder.addMatcher(pubsub_matcher(publish_str), &publish_printer);
+    finder.addMatcher(pubsub_matcher(subscribe_str), &subscribe_printer);
 
     return Tool.run(newFrontendActionFactory(&finder).get());
 }
